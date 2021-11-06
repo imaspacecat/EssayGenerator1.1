eaee5fd1269f3d99c4e201a66b315f1a205d3e3a
imaspacecat <library4lion@gmail.com>
2021-11-06 00:30:38 -0700

    added a bunch of code and methods. There are still tons of bugs.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 import org.fastily.jwiki.core.*;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.regex.*;
+import java.util.Random;
 
 class Main
 {
+    private int var;
+
     public static void main(String[] args){
 
         String articleName = "Cookie";
@@ -12,24 +16,25 @@ class Main
         String content = wiki.getPageText(articleName);
 
 
-        /**
-         iterate over all indexes less then the split array size of the content [i]
+        /*
+         iterate over all indexes less than the split array size of the content [i]
          set the categories' title param to the capture group of the regex
          set the content param to the split(regex) array's index [i]
-         **/
+         */
         int size = 50;
-        ArrayList<Categories> categories = new ArrayList<Categories>(size);
+        ArrayList<Categories> categories = new ArrayList<>(size);
 
         String[] splitArray = content.split("==.*?==");
 
         Pattern pattern = Pattern.compile("==(.*?)==");
         Matcher matcher = pattern.matcher(content);
 
-        /**
+        /*
          to-do:
-         - make it not include ==citations==, ==further reading==, ==external links==, ==general sources==...
          - make sub categories connected somehow to larger categories so when generating the essay it can add those as smaller parts of the main body paragraph. E.g. Maintenance and Repair -> Maintenance, Repair
-         **/
+         - fix extra pair of = when cleaning categories (because of sub categories)
+         - fix ====x==== subcategories being removed
+         */
         int count = 0;
         while (matcher.find()) {
             count++;
@@ -41,6 +46,8 @@ class Main
         int index = 0;
         while (matcher.find()) {
             titles[index] = matcher.group();
+            titles[index] = clean("==(.*?)==", titles[index]);
+            titles[index] = titles[index].trim();
             index++;
         }
 
@@ -49,35 +56,114 @@ class Main
             categories.add(i, (new Categories(titles[i-1], splitArray[i])));
         }
 
+        ArrayList<Categories> cleanCategories = new ArrayList<>(size);
 
-        for (int i = 0; i < categories.size(); i++) {
+        //fix all the bugs in this part including miss alignment between title and content
+        int skipped = 0;
+        for(int i = 0; i < categories.size(); i++){
+            if(
+                    categories.get(i).getTitle().equalsIgnoreCase("citations") ||
+                            categories.get(i).getTitle().equalsIgnoreCase("further reading") ||
+                            categories.get(i).getTitle().equalsIgnoreCase("external links") ||
+                            categories.get(i).getTitle().equalsIgnoreCase("general sources") ||
+                            categories.get(i).getTitle().equalsIgnoreCase("notes") ||
+                            categories.get(i).getTitle().equalsIgnoreCase("references") ||
+                            categories.get(i).getTitle().equalsIgnoreCase("gallery") ||
+                            categories.get(i).getTitle().equalsIgnoreCase("see also"))
+            {
+                skipped++;
+                continue;
+            }
 
-            categories.set(i, new Categories(categories.get(i).getTitle(), categories.get(i).getContent().replaceAll("\\[\\[File.*]]", "")));
-            categories.set(i, new Categories(categories.get(i).getTitle(), clean("\\[\\[[^\\[\\[]*?([^|]*?)\\]\\]", categories.get(i).getContent()+"[[|]]")));
-            categories.set(i, new Categories(categories.get(i).getTitle(), clean("'''(.*?)'''", categories.get(i).getContent()+"''''''")));
-            categories.set(i, new Categories(categories.get(i).getTitle(), clean("''(.*?)''", categories.get(i).getContent()+"''''")));
-            categories.set(i, new Categories(categories.get(i).getTitle(), categories.get(i).getContent().replaceAll("(?s)<ref.*?</ref>", "")));
-            categories.set(i, new Categories(categories.get(i).getTitle(), categories.get(i).getContent().replaceAll("(?s)<ref.*?/>", "")));
-            categories.set(i, new Categories(categories.get(i).getTitle(), categories.get(i).getContent().replaceAll("(?s)<gallery.*?</gallery>", "")));
 
-            categories.set(i, new Categories(categories.get(i).getTitle(), categories.get(i).getContent().replaceAll("\\{\\{.*?}}", "")));
+            cleanCategories.add(i-skipped, new Categories(categories.get(i).getTitle(), categories.get(i).getContent()));
 
 
-            System.out.println("index: " + i + ", title: " + categories.get(i).getTitle() + "\ncontent: \n" + categories.get(i).getContent());
+
         }
+
+
+        for (int i = 0; i < cleanCategories.size(); i++) {
+            String res = cleanCategories.get(i).getContent();
+            res = res.replaceAll("\\[\\[File.*]]", "");
+            res = clean("\\[\\[[^\\[\\[]*?([^|]*?)\\]\\]", res+"[[|]]");
+            res = clean("'''(.*?)'''", res+"''''''");
+            res = clean("''(.*?)''", res+"''''");
+            res = res.replaceAll("(?s)<ref.*?</ref>", "");
+            res = res.replaceAll("(?s)<ref.*?/>", "");
+            res = res.replaceAll("(?s)<gallery.*?</gallery>", "");
+            res = res.replaceAll("(?s)\\{\\{.*?}}", "");
+            res = res.replaceAll("(?s)<!--.*-->", "");
+
+            cleanCategories.set(i, new Categories(cleanCategories.get(i).getTitle(), res));
+
+        }
+
+        //put together essay
+        Random random = new Random();
+        int randIndex = random.nextInt(cleanCategories.size() - 1);
+        String essay = topicSentence(articleName, cleanCategories.get(randIndex).getTitle().toLowerCase()) + cleanCategories.get(randIndex).getContent();
+
+        System.out.println(essay);
+
 
     }
 
-    static String clean(String regex, String content){
+    private static String clean(String regex, String content){
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(content);
-        StringBuffer stringBuffer = new StringBuffer();
+        StringBuilder stringBuffer = new StringBuilder();
 
         while(matcher.find()){
             matcher.appendReplacement(stringBuffer, "$1");
         }
         return stringBuffer.toString();
+    }
+
+    //add subcategories
+    private static String topicSentence(String topic, String title){
+        String[] topicArray = {
+                "The " + title + " of " + topic.toLowerCase() + " is a very interesting topic. ",
+                "There are many cool things to know about the " + title + " of " + topic.toLowerCase() + ". ",
+                topic + " has had a very rich and interesting history. ",
+                "Not many people truly know about the " + title + " of " + topic.toLowerCase() + ". ",
+                "I love the " + title + " of " + topic.toLowerCase() + " as it is very wonderful. ",
+                "Have you heard about the " + title + " of " + topic.toLowerCase() + "? ",
+                "Have you ever thought about the story of the " + title + " of " + topic.toLowerCase() + "? "
+        };
+
+        if(topic.toLowerCase().equalsIgnoreCase(title)){
+            topicArray = new String[]{
+                    topic + " is a very interesting topic. ",
+                    "There are many cool things to know about the " + title + ". ",
+                    topic + " has had a very rich and interesting history. ",
+                    "Not many people truly know about " + title + ". ",
+                    "I love the " + title + " as it is very wonderful. ",
+                    "Have you heard about " + title + "? ",
+                    "Have you ever thought about the story of " + title + "? "
+            };
+        }
+
+        Random random = new Random();
+        int randIndex = random.nextInt(topicArray.length-1);
+        return topicArray[randIndex];
+    }
+
+    private static String conclusionSentence(String topic){
+
+        String[] conclusionArray = {
+                " Now you know all about " + topic.toLowerCase() + ".",
+                " As you can see, " + topic.toLowerCase() + " has a wonderful history.",
+                " I hope that now, when you think of " + topic.toLowerCase() + ", you understand it better.",
+                " And this is why I believe" + topic.toLowerCase() + "is so amazing.",
+                " I hope that by reading this, you have developed a liking for " + topic.toLowerCase() + ".",
+                topic + " is amazing and I hope you can see that now."
+        };
+
+        Random random = new Random();
+        int randIndex = random.nextInt(conclusionArray.length-1);
+        return conclusionArray[randIndex];
     }
 
 
